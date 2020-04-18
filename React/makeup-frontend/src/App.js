35036e1f7f7ccd36afaa7bc98b70870a379df7ab
Akshay Gupta <akshay.gupta@research.iiit.ac.in>
2020-04-18 18:28:13 +0530

    Created Image upload components

--- React/makeup-frontend/src/App.js
+++ React/makeup-frontend/src/App.js
@@ -1,25 +1,45 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import MuiAppBar from '@material-ui/core/AppBar';
+import { Toolbar, Typography, Container, Paper, Grid, Button } from '@material-ui/core';
+import ImageUpload from './ImageUpload'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+      <MuiAppBar position="static" style={{alignItems:'center'}}>
+        <Toolbar>
+          <Typography style={{fontSize:20}}>
+        Make up Transfer
+        </Typography>
+        </Toolbar>
+      </MuiAppBar>
+      <Container maxWidth="lg">
+        <Grid container spacing={3} style={{marginTop:50}}>
+          <Grid item sm={6} xs={12}>
+            <Paper elevation={1}>Original Image
+            <ImageUpload/>
+            </Paper>
+          </Grid>
+
+          <Grid item sm={6} xs={12}>
+            <Paper elevation={1}>Makeup Image
+            <ImageUpload/>
+            </Paper>
+          </Grid>
+
+          <Grid item sm={12} xs={12} alignContent='center'>
+            <Button st>Show</Button>
+          </Grid>
+
+          <Grid item sm={12} xs={12}>
+            <Paper elevation={1}>Result</Paper>
+          </Grid>
+
+
+        </Grid>
+      </Container>
+      </div>
   );
 }
 
