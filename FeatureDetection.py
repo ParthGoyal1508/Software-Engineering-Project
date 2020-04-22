1e3fc2c3f1a0596aacde096d4327d8227d61b700
Akhil singh <akhil1.as42@gmail.com>
2020-04-22 18:33:33 +0530

    Automated the forehead selection

--- FeatureDetection.py
+++ FeatureDetection.py
@@ -17,48 +17,46 @@ def rect_to_bb(rect):
 
 def shape_to_np(feature_points, dtype="int"):
 
-    coords = np.zeros((68, 2), dtype=dtype)
-    for i in range(0, 68):
+    coords = np.zeros((81, 2), dtype=dtype)
+    for i in range(0, 81):
         coords[i] = (feature_points.part(i).x, feature_points.part(i).y)
 
     return coords
 
 class ForeheadCoordinateStore:
     def __init__(self,feature_points,image):
-        print "Select 10 points on forehead (left to right)"
-        cv2.namedWindow('Select_Forehead_Points')
-        cv2.setMouseCallback('Select_Forehead_Points', self.select_point)
-        self.points = np.zeros((78, 2), dtype="int")
-        self.count = 0
-        self.inputFeature = len(feature_points)
+        #print "Select 10 points on forehead (left to right)"
+        #cv2.namedWindow('Select_Forehead_Points')
+        #cv2.setMouseCallback('Select_Forehead_Points', self.select_point)
+        self.points = np.zeros((81, 2), dtype="int")
+        # self.count = 0
+        # self.inputFeature = len(feature_points)
         self.output_marked_image = np.copy(image)
         for i in range(len(feature_points)):
             self.points[i] = feature_points[i]
         for (x, y) in feature_points:
             cv2.circle(self.output_marked_image, (x, y), 2, (0, 0, 255), -1)
-        while self.count != 10:
-            cv2.imshow("Select_Forehead_Points", self.output_marked_image)
-            cv2.waitKey(20)
-        cv2.imshow("Select_Forehead_Points", self.output_marked_image)
-        cv2.waitKey(20)
-        cv2.destroyWindow("Select_Forehead_Points")
+        # while self.count != 10:
+        #     cv2.imshow("Select_Forehead_Points", self.output_marked_image)
+        #     cv2.waitKey(20)
+#)
 
     def getOutputMarkedImage(self):
         return self.output_marked_image
 
-    def select_point(self,event,x,y,flags,param):
-        if event == cv2.EVENT_LBUTTONDOWN:
-            print "Points selected " + str(self.count + 1)
-            cv2.circle(self.output_marked_image, (x, y), 2, (0, 0, 255), -1)
-            self.points[self.inputFeature + self.count] = np.array([x, y], dtype="int")
-            self.count = self.count + 1
-        if self.count == 10:
-            print "Points successfully selected"
+    # def select_point(self,event,x,y,flags,param):
+    #     if event == cv2.EVENT_LBUTTONDOWN:
+    #         print "Points selected " + str(self.count + 1)
+    #         cv2.circle(self.output_marked_image, (x, y), 2, (0, 0, 255), -1)
+    #         self.points[self.inputFeature + self.count] = np.array([x, y], dtype="int")
+    #         self.count = self.count + 1
+    #     if self.count == 10:
+    #         print "Points successfully selected"
 
 def landmark_detection(image):
 
     detector = dlib.get_frontal_face_detector()
-    predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
+    predictor = dlib.shape_predictor("shape_predictor_81_face_landmarks.dat")
     gray_img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
     #Implement warping
--- MakeUp.py
+++ MakeUp.py
@@ -5,71 +5,64 @@ import FeatureDetection
 import wsFilter
 import morphing
 
-def call(a,b):
-
+def call():
     image_height = 500
-    
-    sourceImagePath = a
-    print(sourceImagePath)
-    # sourceImagePath = "./SampleImages/source.jpg"
+    sourceImagePath = "./SampleImages/source.jpg"
     sourceImage = cv2.imread(sourceImagePath)
     r = image_height * 1.0 / sourceImage.shape[0]
     dim = (int(sourceImage.shape[1] * r), image_height)
     sourceImage = cv2.resize(sourceImage, dim, interpolation=cv2.INTER_AREA)
-    
+
     output_image1, feature_points1, triangulation1,K1 = FeatureDetection.landmark_detection(sourceImage)
     sourceTuple = (output_image1, feature_points1, triangulation1,K1)
     Kdash = (K1 - np.min(K1))/(np.max(K1)-np.min(K1))
     # cv2.imshow("Face Mask",Kdash)
-    
-    # makeUpImagePath = "./SampleImages/makeU.jpg"
-    makeUpImagePath = b
+
+    makeUpImagePath = "./SampleImages/makeU.jpg"
+    #makeUpImagePath = b
     makeUpImage = cv2.imread(makeUpImagePath)
     r = image_height * 1.0 / makeUpImage.shape[0]
     dim = (int(makeUpImage.shape[1] * r), image_height)
     makeUpImage = cv2.resize(makeUpImage, dim, interpolation=cv2.INTER_AREA)
     output_image2, feature_points2, triangulation2,K2 = FeatureDetection.landmark_detection(makeUpImage)
     makeUpTuple = (output_image2, feature_points2, triangulation2,K2)
-    
+
     sourceLAB = cv2.cvtColor(sourceImage,cv2.COLOR_BGR2LAB)
     resultant_image = np.copy(sourceLAB)
-    
+
     sourceLightness = sourceLAB[:,:,0]
     sourceColor = sourceLAB[:,:,1:3]
     sourceStructureLayer,sourceTextureLayer = wsFilter.wls_filter(sourceLightness,K1)
-    
+
     makeUpLAB = cv2.cvtColor(makeUpImage,cv2.COLOR_BGR2LAB)
     makeUpLightness = makeUpLAB[:,:,0]
     makeUpColor = makeUpLAB[:,:,1:3]
     makeUpStructureLayer,makeUpTextureLayer = wsFilter.wls_filter(makeUpLightness,K2)
-    
-    
+
+
     deltaInput = 0
     deltaMakeUp = 1
     resultant_skin_detail = morphing.morph_image_delta1_delta2(sourceTextureLayer,makeUpTextureLayer,deltaInput,deltaMakeUp,sourceTuple,makeUpTuple)
-    
+
     alphaFactor = 0.8
     resultant_image[:,:,1:3] = morphing.morph_image_delta1_delta2(sourceColor,makeUpColor,1 - alphaFactor,alphaFactor,sourceTuple,makeUpTuple,True)
-    
+
     scaleFactor = 0.5
     sourceStructureLayer_halved = cv2.resize(sourceStructureLayer,(int(sourceStructureLayer.shape[1]*scaleFactor),int(sourceStructureLayer.shape[0]*scaleFactor)),interpolation=cv2.INTER_AREA)
     dim = (sourceStructureLayer.shape[1],sourceStructureLayer.shape[0])
     sourceStructureLayer_retained_blurred = cv2.resize(sourceStructureLayer_halved,dim,interpolation=cv2.INTER_AREA)
-    
-    
+
+
     laplacian_makeUpStructure = cv2.Laplacian(makeUpStructureLayer,cv2.CV_64F)
-    
+
     resultant_structure_layer = morphing.morph_image_delta1_delta2(sourceStructureLayer_retained_blurred,laplacian_makeUpStructure,1,1,sourceTuple,makeUpTuple)
-    
+
     sum_detail_structure = resultant_skin_detail + resultant_structure_layer
     sum_detail_structure = (sum_detail_structure - np.min(sum_detail_structure))/(np.max(sum_detail_structure)-np.min(sum_detail_structure))
-    
+
     resultant_image[:,:,0] = (sum_detail_structure*255).astype(np.uint8)
-    
+
     resultant_image = cv2.cvtColor(resultant_image,cv2.COLOR_LAB2BGR)
-    cv2.imwrite('out.jpg',np.hstack((sourceImage,resultant_image,makeUpImage)))
+    cv2.imwrite('out.jpg',resultant_image)
     
-    # cv2.imshow("Output",np.hstack((sourceImage,resultant_image,makeUpImage)))
-    # key = cv2.waitKey(0)
-    # if key & 0xFF == ord('s'):
-    #     cv2.imwrite('out.jpg',np.hstack((sourceImage,resultant_image,makeUpImage)))
+call()
